c32576ba0c7ff6ce7baea1df3b46b17a53b143e9
shimobayashi <akimasashimobayashi@gmail.com>
2020-07-26 22:35:04 +0900

    とりあえず兵庫県の釣果情報のURLを一覧できるようになった。

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,19 @@ import * as puppeteer from 'puppeteer';
     slowMo: 0,
   });
   const page = await browser.newPage();
-  await page.goto('https://anglers.jp/');
+
+  await page.goto('https://anglers.jp/prefectures/28/catches');
+  await page.waitForSelector('div.col-6');
+  const elements = await page.$$('div.col-6');
+  for (const element of elements) {
+    const anchor = await element.$('a');
+    if (!anchor) continue;
+
+    const url = (await (await anchor.getProperty('href')).jsonValue()) + '';
+    console.log(url);
+  }
+
+  await browser.close();
 })().catch(error => {
   console.error(error);
   throw error;

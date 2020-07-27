3edb6eecc8d59eaced8270853ddfa5b5efdc93b7
shimobayashi <akimasashimobayashi@gmail.com>
2020-07-27 17:20:19 +0900

    vimagemoreに投稿成功した。

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as puppeteer from 'puppeteer';
+import axios from 'axios';
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -14,26 +15,36 @@ import * as puppeteer from 'puppeteer';
     // リンク
     const anchor = await element.$('a');
     const link = (await (await anchor!.getProperty('href')).jsonValue()) + '';
-    console.log(link);
 
-    // タイトル
+    // タイトルとタグ
     const detail1 = await element.$(
       '.card-body > div:nth-child(3) > div:nth-child(1)'
     );
-    const fish =
+    let fish =
       (await (await detail1!.getProperty('innerText')).jsonValue()) + '';
-    console.dir(fish.trim());
+    fish = fish.trim();
 
     const detail2 = await element.$('.card-body > div:nth-child(4)');
-    const area =
+    let area =
       (await (await detail2!.getProperty('innerText')).jsonValue()) + '';
-    console.dir(area.trim());
+    area = area.trim();
+
+    const title = area + ' - ' + fish;
+    const tags = ['釣果情報', '兵庫県', area, fish];
 
     // 画像blob
     const image = await element.screenshot({
       encoding: 'base64',
       type: 'jpeg',
       quality: 60,
+    });
+
+    await postToVimagemore({
+      id: link,
+      title: title,
+      tags: tags,
+      link: link,
+      image: image,
     });
   }
 
@@ -42,3 +53,30 @@ import * as puppeteer from 'puppeteer';
   console.error(error);
   throw error;
 });
+
+interface Params {
+  id: string;
+  title: string;
+  tags: string[];
+  link: string;
+  image: string;
+}
+async function postToVimagemore(params: Params) {
+  console.dir({
+    id: params.id,
+    title: params.title,
+    tags: params.tags,
+    link: params.link,
+  });
+
+  // 情報をまとめてアップロードする
+  return axios
+    .post(process.env.VIMAGEMORE_UPLOADER_URL ?? '', params)
+    .then(ret => {
+      console.log(ret.status);
+    })
+    .catch(error => {
+      // 重複したidを指定しているとエラーが返ってくるのでガンガンエラーが流れてくるはず
+      console.error(error.message);
+    });
+}
